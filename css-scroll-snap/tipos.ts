33ab52adc82f6466ce85e9250d75fc466f0f600d
Cassiano Scheidemantel <cassiano@scheidemantel.com.br>
2024-07-06 11:04:44 -0300

    fix: adicionado novos reels

---
+++ css-scroll-snap/tipos.ts
@@ -0,0 +1,14 @@
+type Carro = {
+  nome: string;
+  subtipos: string;
+  carroceria: number;
+  potencia: number[];
+  preco: number;
+  motor: string;
+  marca: Marcas;
+};
+
+type Marcas = {
+  nome;
+  nacionalidade;
+};
